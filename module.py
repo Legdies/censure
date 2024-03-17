067f7c991b5f049b2805004e2cbc4fe9ca920c28
LeGDieS <legdies@gmail.com>
2024-03-17 13:56:44 +0000

    created repository

--- module.py
+++ module.py
@@ -7,14 +7,14 @@ class Tester:
         parsed = []
 
         with open("dictionary.txt", "r") as file:
-            # Читаем содержимое файла и разбиваем его на слова
+
             words = file.read().split()
 
-        # Создаем множество для быстрой проверки принадлежности слова к словарю
+
         cleaned_words = set()
         for word in words:
             cleaned_word = ''.join(filter(str.isalnum, word))
-            cleaned_words.add(cleaned_word.lower())  # Приводим слова к нижнему регистру для удобства сравнения
+            cleaned_words.add(cleaned_word.lower())
 
         try:
             for word in parsing:
